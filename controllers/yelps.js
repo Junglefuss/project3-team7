9f8593d31cfbee2f28e132a1a781cd758d4c4f8a
Terence <xmangahas@sbcglobal.net>
2019-10-22 14:24:47 -0500

    API working

---
+++ controllers/yelps.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const router = express.Router();
+
+let yelpApi = require('yelp-api');
+
+// Create a new yelpAPI object with your API key
+let apiKey = 'iyEkKM7x92L8Mib8tCO0wRP8hUbwpc4xbAlyF_slEjQjcvc01lK0ytRA6VI81ogvn8aTuwIV7TMxTN_c7G48BAXQVOEZ5MCBd6PqNgGq5uq9P6jd3sQ7SHwU0vysXXYx';
+let yelp = new yelpApi(apiKey);
+
+
+
+router.get('/:location', (req, res) => {
+    let params = [
+        { location: req.body.location,
+          term: 'bars',  
+          limit: 3  
+        },
+    ];
+    
+    // Call the endpoint
+    yelp.query('businesses/search', params)
+    .then(data => {
+      // Success
+      res.json(data);
+      console.log(data);
+    })
+    .catch(err => {
+      // Failure
+      console.log(err);
+    });
+});
+
+module.exports = router;
---
+++ yelp-api/yelpAPI.js
@@ -0,0 +1,26 @@
+// let yelpApi = require('yelp-api');
+
+// // Create a new yelpAPI object with your API key
+// let apiKey = 'iyEkKM7x92L8Mib8tCO0wRP8hUbwpc4xbAlyF_slEjQjcvc01lK0ytRA6VI81ogvn8aTuwIV7TMxTN_c7G48BAXQVOEZ5MCBd6PqNgGq5uq9P6jd3sQ7SHwU0vysXXYx';
+// let yelp = new yelpApi(apiKey);
+
+// // Set any parameters, if applicable (see API documentation for allowed params)
+// let params = [
+//     { location: 'Washington DC',
+//       term: 'bars',  
+//       limit: 3  
+//     },
+// ];
+
+// // Call the endpoint
+// yelp.query('businesses/search', params)
+// .then(data => {
+//   // Success
+//   console.log(data);
+// })
+// .catch(err => {
+//   // Failure
+//   console.log(err);
+// });
+
+// module.exports = yelpApi
