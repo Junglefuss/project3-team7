e99a4b1c3157aa5fc524cfad599b9bead7c66b02
Jonathan Goldfuss <jgoldfuss@coproductions.com>
2019-10-22 17:36:03 -0400

    debugging api

--- controllers/yelps.js
+++ controllers/yelps.js
@@ -4,21 +4,16 @@ const router = express.Router();
 let yelpApi = require('yelp-api');
 
 // Create a new yelpAPI object with your API key
-let apiKey = 'iyEkKM7x92L8Mib8tCO0wRP8hUbwpc4xbAlyF_slEjQjcvc01lK0ytRA6VI81ogvn8aTuwIV7TMxTN_c7G48BAXQVOEZ5MCBd6PqNgGq5uq9P6jd3sQ7SHwU0vysXXYx';
+let apiKey =
+  'iyEkKM7x92L8Mib8tCO0wRP8hUbwpc4xbAlyF_slEjQjcvc01lK0ytRA6VI81ogvn8aTuwIV7TMxTN_c7G48BAXQVOEZ5MCBd6PqNgGq5uq9P6jd3sQ7SHwU0vysXXYx';
 let yelp = new yelpApi(apiKey);
 
-
-
 router.get('/:location', (req, res) => {
-    let params = [
-        { location: req.body.location,
-          term: 'bars',  
-          limit: 3  
-        },
-    ];
-    
-    // Call the endpoint
-    yelp.query('businesses/search', params)
+  let params = [{ location: req.params.location, term: 'bars', limit: 5 }];
+
+  // Call the endpoint
+  yelp
+    .query('businesses/search', params)
     .then(data => {
       // Success
       res.json(data);
